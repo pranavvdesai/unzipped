14230e9645ced7b2cdb711a575f7e6846e532efa
pranavvdesai <pranavdesai.psd@gmail.com>
2021-05-01 22:59:42 +0530

    added dashboard

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect,session,flash,redirect,url_for
-import os
 from passlib.hash import sha256_crypt
 import mysql.connector
+import os
 import requests
 import cv2
 from flask import Response
@@ -13,18 +13,23 @@ import numpy as np
 
 app= Flask(__name__)
 app.secret_key=os.urandom(24)
-
-conn=mysql.connector.connect(host="remotemysql.com",user="9YwiYaINDg",password="**********",database="9YwiYaINDg")
+ 
+conn=mysql.connector.connect(host="remotemysql.com",user="9YwiYaINDg",password="WB2u9rVHb5",database="9YwiYaINDg")
 cursor=conn.cursor()
+
 
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
      return render_template('home.html')
 
+
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
 
 @app.route('/login')
 def login():
@@ -33,9 +38,11 @@ def login():
 @app.route('/home')
 def home():
     if 'user_id' in session:
-        return render_template('home.html',displayname=displayname)
+        return render_template('home.html')
     else:
         return redirect('/')
+   
+
 @app.route("/loginvalidation", methods=['POST'])
 def loginval():
     global email
@@ -67,6 +74,14 @@ def loginval():
         error="invalid email.register"
         return render_template('login.html',error=error)
 
+
+    
+
+
+
+    
+
+
 @app.route('/add_user',methods=['POST'])
 def add_user():
     global name    
@@ -83,11 +98,47 @@ def add_user():
     flash('accoount created')
     return redirect('/login')
 
+
 @app.route('/logout')
 def logout():
     session.pop('user_id')
     return redirect('/')
 
+
+
+@app.route('/home', methods=['POST'])
+def form_data():
+    global result
+    if request.method=="POST":
+        req=request.form
+        movie=req['movie']
+        music=req['music']
+        sport=req['sport']
+        choice=req['choice']
+        result=movie+music+sport+choice
+        cursor.execute("""SELECT * FROM `userfashion`  WHERE `email` LIKE '{}' AND `password` LIKE '{}' """.format(email,password))
+        user=cursor.fetchone()
+        cursor.execute("""UPDATE `userfashion` set results='{}' WHERE email='{}' """.format(result,email))
+        conn.commit()
+        conn.commit()
+        return render_template('home.html')
+
+
+
+
+
+
+
+@app.route('/image1')
+def image1():
+    return render_template('image1.html')
+
+@app.route('/image2')
+def image2():
+    return render_template('image2.html')
+
+
+    
 @app.route('/video1_feed')
 def video1_feed():
     return render_template('video1.html')
@@ -103,9 +154,7 @@ def video1():
 @app.route('/video2')
 def video2():
 	return render_template("video2.html")
-@app.route('/image1')
-def image1():
-    return render_template('image1.html')
+
 
 
 
@@ -119,6 +168,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/image1', methods=['POST'])
 def upload_image1():
@@ -137,7 +187,7 @@ def upload_image1():
         np.set_printoptions(suppress=True)
         model = tensorflow.keras.models.load_model('keras_model.h5')
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        image = Image.open(r'static\uploads\pose2.jpg')
+        image = Image.open(r'/static/uploads/post2.jpeg')
         size = (224, 224)
         image = ImageOps.fit(image, size, Image.ANTIALIAS)
         image_array = np.asarray(image)
@@ -150,6 +200,8 @@ def upload_image1():
     else:
         flash('Allowed image types are - png, jpg, jpeg, gif')
         return redirect(request.url)
+
+
 
 @app.route('/image2', methods=['POST'])
 def upload_image2():
@@ -168,7 +220,7 @@ def upload_image2():
         np.set_printoptions(suppress=True)
         model = tensorflow.keras.models.load_model('keras_model.h5')
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        image = Image.open(r'static\uploads\pose1.jpeg')
+        image = Image.open(r'/static/uploads/post1.jpeg')
         size = (224, 224)
         image = ImageOps.fit(image, size, Image.ANTIALIAS)
         image_array = np.asarray(image)
@@ -183,9 +235,15 @@ def upload_image2():
         return redirect(request.url)
 
 
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
+
+
 
 
 @app.route('/admin_add',methods=['POST'])
@@ -201,7 +259,34 @@ def admin_add():
     return redirect(url_for('home'))
 
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('userdashboard.html')
+
+
+@app.route('/dashboard_add',methods=['POST'])
+def dashboard_add():
+    location=request.form.get('searchlocation')
+    cursor.execute("""SELECT * FROM `admindatabase` WHERE `location` LIKE '{}'""".format(location))
+    posts=cursor.fetchall()
+    print(posts)
+    return render_template('userdashboard.html',posts=posts)
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# error handler
+
+# @app.errorhandler(404)
+# def error_handler_page(e):
+#     return render_template('error.html')
